252075271f3028a936bdbffc6822d246ae804ec1
CJC <cjc0722hz@gmail.com>
2016-09-20 19:12:36 -0700

    improve the database structure and add some basic functions in Group

--- BookKeeper/models.py
+++ BookKeeper/models.py
@@ -2,29 +2,53 @@ from django.db import models
 
 
 class Group(models.Model):
-    group_id = models.IntegerField(primary_key=True)
-    group_name = models.CharField(max_length=20)
+    group_name = models.CharField(max_length=20, unique=True)
+    total_expense = models.IntegerField(default=0)
 
     def __str__(self):
-        return 'Group No.{}'.format(self.group_id)
+        return 'Group {}'.format(self.group_name)
+
+    class Meta:
+        ordering = ('group_name',)
+
+    def add_a_person(self, person):
+        self.person_set.add(person)
+        person.groups.add(self)
+        new_record = Record(group=self, person=person, amount=0)
+        new_record.save()
+        self.record_set.add(new_record)
+        self.save()
+
+    # add a new bill record into the group
+    def record_a_bill(self, bill):
+        self.bill_set.add(bill)
+        self.total_expense += bill.price
+        self.save()
 
 
 class Person(models.Model):
-    groups = models.ManyToManyField(Group)
-    person_id = models.IntegerField(primary_key=True)
-    name = models.CharField(max_length=20)
+    groups = models.ManyToManyField(Group) # one person can be in many groups at a time
+    name = models.CharField(max_length=20, unique=True)
 
     def __str__(self):
-        return 'Person id{}'.format(self.person_id)
+        return 'Person name:{}'.format(self.name)
+
+    class Meta:
+        ordering = ('name',)
 
 
 class Bill(models.Model):
     group = models.ForeignKey(Group, on_delete=models.CASCADE)
     payer = models.ForeignKey(Person, related_name='payer')
     consumers = models.ManyToManyField(Person, related_name='consumers')
-    bill_id = models.IntegerField(primary_key=True)
     price = models.IntegerField()
     bill_description = models.TextField(max_length=200)
 
     def __str__(self):
-        return 'Bill No.{} with price {}'.format(self.bill_id, self.price)
+        return 'Bill with price {}'.format(self.price)
+
+
+class Record(models.Model):
+    group = models.ForeignKey(Group, on_delete=models.CASCADE)
+    person = models.ForeignKey(Person, on_delete=models.CASCADE)
+    amount = models.IntegerField(default=0)
--- BookKeeper/tests.py
+++ BookKeeper/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
 
-# Create your tests here.
+from .models import Group, Person, Record, Bill
+
+
+class GroupMethodsTest(TestCase):
+    def test_add_a_person(self):
+        g = Group(group_name='test')
+        g.save()
+        p = Person(name='test')
+        p.save()
+        g.add_a_person(p)
+        self.assertEqual(g.record_set.count(), 1)
+        self.assertEqual(g.record_set.all()[0].group, g)
+        self.assertEqual(g.record_set.all()[0].person, p)
+        self.assertEqual(p.groups.all()[0], g)
